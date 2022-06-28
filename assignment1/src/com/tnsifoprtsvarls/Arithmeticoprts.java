32ecd0eca05853dc8b5b02653376cc679925017b
Shrujana <Shrujana@Sonu-AVB>
2022-06-28 13:02:17 +0530

    assignment1

---
+++ assignment1/src/com/tnsifoprtsvarls/Arithmeticoprts.java
@@ -0,0 +1,13 @@
+package com.tnsifoprtsvarls;
+
+public class Arithmeticoprts {
+
+	public static void main(String[] args) {
+		
+		int x = 10, y=20;
+		System.out.println(x+y); //30
+		System.out.println(--x); // 9
+		System.out.println(++y); //21
+	}
+
+}
